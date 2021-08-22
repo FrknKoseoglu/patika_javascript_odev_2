919ce45e8d7a0fba348a749d38db47f26b9e1105
Furkan Köseoğlu <furkan.koseoglu@rhinorunner.com>
2021-08-22 21:30:28 +0300

    - toast bildirim - silme

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let li = document.querySelectorAll("#list li")
 let list = document.querySelector("#list")
+let li_after = document.querySelectorAll("#list li:after")
 let lsArray = []
 let ls = localStorage.getItem("li")
 
@@ -9,9 +10,9 @@ const localStrogeFill = () => {
         list.innerHTML = ""
         lsArray = JSON.parse(ls)
         lsArray.forEach(item => {
-            let isCheck
-            item.isActive ? isCheck = "checked" : isCheck = ""
-            list.innerHTML += `<li class="${isCheck}">${item.text}</li>`
+                let isCheck
+                item.isActive ? isCheck = "checked" : isCheck = ""
+                list.innerHTML += `<li class="${isCheck}"><p>${item.text}</p><span>✖</span></li>`
         })
         li = document.querySelectorAll("#list li")
     }
@@ -20,39 +21,62 @@ const localStrogeFill = () => {
 const liClick = () => {
     li = document.querySelectorAll("#list li")
     for (let i = 0; i < li.length; i++) {
-        li[i].addEventListener("click", () => {
-            liClass = li[i].classList.toString()
-            if(liClass.includes("checked")){
-                li[i].classList.remove("checked")
-                lsArray[i].isActive = false
-            }
-            else{
-                li[i].classList.add("checked")
-                lsArray[i].isActive = true
-            }
-            localStorage.setItem("li", JSON.stringify(lsArray))
-        })
+            li[i].querySelector("span").addEventListener("click", () => {
+                lsArray[i] = null
+                lsArray = lsArray.filter(e => e != null)
+                li[i].classList.add("d-none")
+                localStorage.setItem("li", JSON.stringify(lsArray))
+            })
+            li[i].querySelector("p").addEventListener("click", () => {
+                liClass = li[i].classList.toString()
+                if (liClass.includes("checked")) {
+                    li[i].classList.remove("checked")
+                    lsArray[i].isActive = false
+                } else {
+                    li[i].classList.add("checked")
+                    lsArray[i].isActive = true
+                }
+                localStorage.setItem("li", JSON.stringify(lsArray))
+            })
+     
     }
+    // for (let i = 0; i < li.length; i++) {
+    //     li[i].after.addEventListener("click", () => {
+    //         lsArray[i].remove()
+    //     })
+    // }
 }
-
 const newElement = () => {
     let getText = document.querySelector("#task").value
-
     if (!getText) {
-        alert("Lütfen bir To-Do giriniz!")
+        $('#Toast-Error').toast('show')
     } else {
+        $('#Toast-Success').toast('show')
         let radioBtn = document.querySelectorAll(".radio-area input:checked")
         if (radioBtn[0].value == "first") {
-            lsArray.unshift({text: getText, isActive: false})
-            list.innerHTML = `<li>${getText}</li>` + list.innerHTML
+            lsArray.unshift({
+                text: getText,
+                isActive: false
+            })
+            list.innerHTML = `<li><p>${getText}</p><span>✖</span></li>` + list.innerHTML
         } else {
-            lsArray.push({text: getText, isActive: false})
-            list.innerHTML += `<li>${getText}</li>`
+            lsArray.push({
+                text: getText,
+                isActive: false
+            })
+            list.innerHTML += `<li><p>${getText}</p><span>✖</span></li>`
         }
         localStorage.setItem("li", JSON.stringify(lsArray))
         liClick()
     }
 }
+
+document.querySelector("#clearBtn").addEventListener("click", () =>{
+    localStorage.clear()
+    list.innerHTML = ""
+    $('#Toast-Clear').toast('show')
+})
+
 
 //Page Load
 localStrogeFill()
